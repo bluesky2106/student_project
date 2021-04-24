c6b099a1859c1015eee1cbb9630b6a5b00a46b9f
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2021-04-24 14:47:19 +0700

    add templates

--- student_register/views.py
+++ student_register/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def student_list(request):
+  return render(request, "student_register/student_list.html")
+
+def student_form(request):
+  return render(request, "student_register/student_form.html")
+
+def student_delete(request):
+  return
